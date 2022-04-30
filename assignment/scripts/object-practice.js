e9b9113dc0a5f5d89e60f6b0d66bd5a3050cf26a
Stephen Womack <swomack111@gmail.com>
2022-04-29 23:50:36 -0500

    created object 'me', created fullName variable, logged first and last favorite foods

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,6 +10,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Stephen',
+  lastName: 'Womack',
+  hasSiblings: false,
+  shoeCount: 5,
+  faveThreeFoods: ['pizza', 'dumplings', 'tacos']
 };
 console.log('A little about me:', me);
 
@@ -19,6 +24,8 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+fullName = me.firstName + ` ` + me.lastName;
+console.log(fullName);
 
 
 
@@ -26,8 +33,8 @@ console.log('A little about me:', me);
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
-
+console.log(`my first favorite food is: ${me.faveThreeFoods[0]}`);
+console.log(`my final favorite food is: ${me.faveThreeFoods.slice(-1)}`);  //found a couple of ways to do this, this way seemed the most elegant? -Stephen
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -35,7 +42,7 @@ console.log('A little about me:', me);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-
+console.log(`my shoe count is: ${me.shoeCount}`)
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
